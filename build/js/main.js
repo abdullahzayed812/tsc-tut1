3943f486e2ab36e2c374a32ee60733f79ffddaa6
Abdullah_Zayed <abdullah@quadratechsoft.com>
2023-01-23 21:43:49 +0200

    add typescript starter lesson

---
+++ build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello TypeScript...");
+let a = 10;
+let b = 5;
+let c = 2;
+console.log(a / b);
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+console.log("Hello TypeScript...");
+
+let a = 10;
+let b = 5;
+let c = 2;
+
+console.log(a / b);
